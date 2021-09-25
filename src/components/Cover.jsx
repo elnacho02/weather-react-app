5b4aa7776d183c26c4f3515068bcae2faba656b9
Ignacio Aranda <87491116+elnacho02@users.noreply.github.com>
2021-09-25 00:10:03 -0300

    asd

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -4,21 +4,17 @@ import porDefecto from "../media/video.mp4";
 import nublado from "../media/nublado.mp4";
 import soleado from "../media/soleado.mp4";
 import lluvia from "../media/lluvia.mp4";
+import noche from "../media/Beautiful Moon Rises through Night Clouds.mp4";
 export default function Cover(props) {
     
     var coverVideo = porDefecto;
     /* soleado */
-    if(props.img==="02d" || props.img===""){
-        coverVideo=soleado;
-    }
+    if(props.img==="02d" || props.img==="")coverVideo=soleado;
     /* lluvioso */
-    else if(props.img==="05d"||props.img==="50d" || props.img==="10d" || props.img==="10n" || props.img==="09n"){
-        coverVideo=lluvia;
-    }
+    else if(props.img==="05d"||props.img==="50d" || props.img==="10d" || props.img==="10n" || props.img==="09n")coverVideo=lluvia;
     /*nublado*/
-    else if(props.img==="04d"||props.img==="04n"||props.img==="03n"){
-        coverVideo=nublado;
-    }
+    else if(props.img==="04d"||props.img==="04n"||props.img==="03n")coverVideo=nublado;
+    else if(props.img ==="01n") coverVideo=noche;
     return (
         <div className="cover-container">
             <video  classname="video" src={coverVideo} autoPlay loop muted></video>
